5e71ef0bb0ffb49024c841431d3c60a55165f324
Aneta Grochowska <aneta.anette.g3@gmail.com>
2022-11-26 13:20:58 +0100

    changes

---
+++ script.js
@@ -0,0 +1,21 @@
+/* import $ from "https://cdn.skypack.dev/jquery@3.6.1";
+
+class Clicked {
+	constructor() {
+
+		$( '.one__button' ).on( 'click', ( e ) => {
+			this.clickIt( e );
+		});
+	}
+  
+  clickIt( e )  {
+    const one = $( e.target.closest( '.one' ) );
+
+    
+    one.toggleClass( 'one--clicked' );
+  }
+}
+
+new Clicked();
+
+*/
